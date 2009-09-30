9137b9a784f706077022d0596b5f9165131496cf
Erik L. Vonderscheer <evonder@gmail.com>
2009-09-30 02:17:24 +0000

    Remove Globals
    Added Locale.lua

--- Broker_TankStats.lua
+++ Broker_TankStats.lua
@@ -1,18 +1,11 @@
-local L_DEF = " ".. _G["COMBAT_RATING_NAME2"]
-local L_DODGE = " ".. _G["COMBAT_RATING_NAME3"]
-local L_PARRY = " ".. _G["COMBAT_RATING_NAME4"]
-local L_BOCK = " ".. _G["COMBAT_RATING_NAME5"]
-local L_EXP = " ".. _G["COMBAT_RATING_NAME24"]
-local L_AVOI = " Avoidance"
-
 local TS = CreateFrame("Frame", "Broker_TankStats")
 local LDB = LibStub("LibDataBroker-1.1")
-TS.Dodge = LDB:NewDataObject("Dodge", {text = "0%"..L_DODGE, value = "0%", suffix = L_DODGE})
-TS.Bock = LDB:NewDataObject("Block", {text = "0%"..L_BOCK, value = "0%", suffix = L_BOCK})
-TS.Parry = LDB:NewDataObject("Parry", {text = "0%"..L_PARRY, value = "0%", suffix = L_PARRY})
-TS.Def = LDB:NewDataObject("Defense", {text = "0"..L_DEF, value = "0", suffix = L_DEF})
-TS.Exp = LDB:NewDataObject("Expertise", {text = "0"..L_EXP, value = "0", suffix = L_EXP})
-TS.Avoi = LDB:NewDataObject("Avoidance", {text = "0%"..L_AVOI, value = "0%", suffix = L_AVOI})
+TS.Dodge = LDB:NewDataObject("Dodge", {text = "0%"..L["DODGE"], value = "0%", suffix = L["DODGE"]})
+TS.Bock = LDB:NewDataObject("Block", {text = "0%"..L["BOCK"], value = "0%", suffix = L["BOCK"]})
+TS.Parry = LDB:NewDataObject("Parry", {text = "0%"..L["PARRY"], value = "0%", suffix = L["PARRY"]})
+TS.Def = LDB:NewDataObject("Defense", {text = "0"..L["DEF"], value = "0", suffix = L["DEF"]})
+TS.Exp = LDB:NewDataObject("Expertise", {text = "0"..L["EXP"], value = "0", suffix = L["EXP"]})
+TS.Avoi = LDB:NewDataObject("Avoidance", {text = "0%"..L["AVOI"], value = "0%", suffix = L["AVOI"]})
 LDB = nil
 
 local format = string.format
@@ -30,27 +23,27 @@ TS:SetScript("OnEvent", function(self, event, unit)
 	if unit and unit ~= player then return end
 
 	local dodge = format(two_fp, GetDodgeChance())
-	self.Dodge.text = dodge..L_DODGE
+	self.Dodge.text = dodge..L["DODGE"]
 	self.Dodge.value = dodge
 
 	local bock = format(two_fp, GetBlockChance())
-	self.Bock.text = bock..L_BOCK
+	self.Bock.text = bock..L["BOCK"]
 	self.Bock.value = bock
 
 	local parry = format(two_fp, GetParryChance())
-	self.Parry.text = parry..L_PARRY
+	self.Parry.text = parry..L["PARRY"]
 	self.Parry.value = parry
 
 	local base, modifier = UnitDefense(player)
 	local def = format(single, base+modifier)
-	self.Def.text = def..L_DEF
+	self.Def.text = def..L["DEF"]
 	self.Def.value = def
 
 	local Exp = format(single, GetCombatRating(24))
-	self.Exp.text = Exp..L_EXP
+	self.Exp.text = Exp..L["EXP"]
 	self.Exp.value = Exp
 	
 	local avoi = format(two_fp, GetDodgeChance() + GetParryChance() + 5 + 1/(0.0625 + 0.956/(GetCombatRating(CR_DEFENSE_SKILL)/4.91850*0.04)))
-	self.Avoi.text = bock..L_AVOI
+	self.Avoi.text = bock..L["AVOI"]
 	self.Avoi.value = avoi	
 end)
---
+++ Locale.lua
@@ -0,0 +1,15 @@
+--[[
+File Author: @file-author@
+File Revision: @file-revision@
+File Date: @file-date-iso@
+]]--
+local debug = false
+--@debug@
+debug = true
+--@end-debug@
+
+local L =  LibStub("AceLocale-3.0"):NewLocale("Broker_TankStats", "enUS", true)
+if L then
+--@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="subtable")@
+if GetLocale() == "enUS" then return end
+end
